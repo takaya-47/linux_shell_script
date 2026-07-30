778a9995209f9c2ee8808150fbbe9e903b893ec4
takaya-47 <local.s.s.47@gmail.com>
2026-07-30 09:44:46 +0900

    関数に配列を渡し、関数で配列を受け取る方法を確認

---
+++ array_function.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+myfunc () {
+    echo "$@" # 配列全体を受け取る
+    echo "$2" # 受け取った配列の2番目を受け取る（添え字だと１に該当するもの）
+}
+
+ARRAY=(1 3 5)
+echo "array is ${ARRAY[*]}"
+echo "second is ${ARRAY[1]}"
+myfunc "${ARRAY[@]}"
