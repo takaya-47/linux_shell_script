16ad88d568f1fe8800612b3f684f5e8b2e8711ed
takaya-47 <local.s.s.47@gmail.com>
2026-08-06 09:16:48 +0900

    recursiveなスクリプト（階乗）

---
+++ factorial.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+factorial() {
+    local n=$1
+    if [ "$n" -le 1 ]; then
+        echo 1
+    else
+        local prev=$(factorial $((n - 1)))
+        echo $((n * prev))
+    fi
+}
+
+read -p "Enter a number: " num
+result=$(factorial "$num")
+echo "Factorial of $num is $result"
+
+
+
+# 4, 3, 2, 1
+# echo 1
+# echo 2 * 1 = 2
+# echo 3 * 2 = 6
+# echo 4 * 6 = 24
